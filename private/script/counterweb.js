3730c229eb7ab07915fc6fbd70c6e335e096b1e7
Viitorsoares <viitorgab.soares@gmail.com>
2025-08-24 18:36:18 -0300

    Finalizando o modulo 1 do ppi

---
+++ private/script/counterweb.js
@@ -0,0 +1,16 @@
+let amount = 0;
+let total = 0;
+let pricePerClick = 1499; 
+
+const btn = document.getElementById("btn")
+const amountSpan = document.getElementById("quantidade")
+const valueSpan = document.getElementById("valor")
+
+btn.addEventListener("click", () => {
+    amount ++;
+    total = amount * pricePerClick;
+
+    amountSpan.textContent = amount;
+    valueSpan.textContent = total.toFixed(2);
+})
+
